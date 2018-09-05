b8fda01df86d18a673262ae2f275f6bcd368612b
RAVISAM <RAVISAM@RAVISAM>
2018-09-05 15:50:57 +0530

    2nd file change

--- src/com/app/model/Employee.java
+++ src/com/app/model/Employee.java
@@ -2,23 +2,34 @@ package com.app.model;
 
 public class Employee {
 private int empId;
+private String empName;
+private double empSal;
 
-public Employee(int empId) {
-	super();
-	this.empId = empId;
-}
 
 public int getEmpId() {
 	return empId;
 }
-
 public void setEmpId(int empId) {
 	this.empId = empId;
 }
 
+
+public String getEmpName() {
+	return empName;
+}
+public void setEmpName(String empName) {
+	this.empName = empName;
+}
+public double getEmpSal() {
+	return empSal;
+}
+public void setEmpSal(double empSal) {
+	this.empSal = empSal;
+}
 @Override
 public String toString() {
-	return "Employee [empId=" + empId + "]";
+	return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
 }
+
 
 }
